13d8ecccd1d2e4c750f3c3eec4dd74a947cc6b30
Ahmed Abdelhafez <ahmedhafez.ta@gmail.com>
2023-10-07 11:38:10 -0700

    Make some fixes to make the code matches Betty styles

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,10 @@
 /* more headers goes here */
 
 /* betty style doc for function main goes there */
+/**
+ * main - Checks if the generated randon number lower than, equal and greater than 0;
+ * Return: 0;
+*/
 int main(void)
 {
 	int n;
@@ -11,9 +15,9 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* Your code goes here */
-	if(n < 0)
+	if (n < 0)
 		printf("%d is negative\n",n);
-	else if(n == 0)
+	else if (n == 0)
 		printf("%d is zero\n",n);
 	else
 		printf("%d is positive\n",n);
