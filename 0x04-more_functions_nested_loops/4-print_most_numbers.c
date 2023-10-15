a5452fa14aeb4074e0d6cfad67c237870c4090bd
Ahmed Abdelhafez <ahmedhafez.ta@gmail.com>
2023-10-15 11:40:50 -0700

    Fixing Bugs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,13 +8,13 @@
  */
 void print_most_numbers(void)
 {
-	char ch;
-	for (ch = '1'; ch <= '9'; ch++)
-	{
-		if ((ch != 2) && (ch != 4))
-		{
-			_putchar(ch);
-		}
-	}
-	_putchar('\n');
+char ch;
+for (ch = '1'; ch <= '9'; ch++)
+{
+if ((ch != 2) && (ch != 4))
+{
+_putchar(ch);
+}
+}
+_putchar('\n');
 }
