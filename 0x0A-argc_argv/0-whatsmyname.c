129d617325e93288801812adf27c1699cce4b1f5
Ahmed Abdelhafez <ahmedhafez.ta@gmail.com>
2023-11-01 01:02:45 -0700

    Fix Some Bugs

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - print the program name
  *
  * @argc: how many arguments that the program takes
- * @*argv: a pointer that points to the program arguments
+ * @argv: a pointer that points to the program arguments
+ *
+ * Return: Always returns 0
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 
