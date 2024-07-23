7c0368c6044ac8f44d8f726763ab7b1a0db6d366
Ahmed Abdelhafez <ahmedhafez.ta@gmail.com>
2024-07-24 01:20:20 +0300

    Debugging

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -23,22 +23,34 @@ void print_times_table(int n)
 				if (result < 10)
 				{
 					_putchar((result % 10) + '0');
+					if (j < n)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+					}
 				}
 				else if ((result > 9) && (result < 100))
 				{
 					_putchar((result / 10) + '0');
 					_putchar((result % 10) + '0');
+					if (j < n)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+					}
 				}
 				else
 				{
 					_putchar((result / 100) + '0');
 					_putchar((result / 10) % 10 + '0');
 					_putchar((result % 10) + '0');
-				}
-				if (j < n)
-				{
-					_putchar(',');
-					_putchar(' ');
+					if (j < n)
+					{
+						_putchar(',');
+						_putchar(' ');
+					}
 				}
 			}
 			_putchar('\n');
