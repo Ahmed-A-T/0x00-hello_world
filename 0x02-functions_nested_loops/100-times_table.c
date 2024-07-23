5375025a4fc44e8b3a2d4140cbe29ca86cd32f5d
Ahmed Abdelhafez <ahmedhafez.ta@gmail.com>
2024-07-24 00:43:51 +0300

    Using _putchar function instead of standard library

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,20 +13,51 @@
 void print_times_table(int n)
 {
 	int i, j, result = 0;
-
-	if ((n <= 15) && (n >= 0))
+	if (n >= 0 && n <= 15)
 	{
-		for (i = 0; i <= n; i++)
+		for (i = 0; i < number; i++)
 		{
-			for (j = 0; j <= n; j++)
+			for (j = 0; j < number; j++)
 			{
 				result = i * j;
-				if (j == n)
-					printf("%d", result);
-				else
-					printf("%d,  ", result);
+				if (result < 10)
+				{
+					_putchar((result % 10) + '0');
+					if (j < number)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+
+					}
+				}
+				else if (result > 9 && result < 100)
+				{
+					_putchar((result / 10) + '0');
+					_putchar((result % 10) + '0');
+					if (j < number)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+
+					}
+				}
+				else if (result > 99)
+				{
+					_putchar((result / 100) + '0');
+					_putchar((result / 10) % 10 + '0');
+					_putchar((result % 10) + '0');
+					if (j < number)
+					{
+						_putchar(',');
+						_putchar(' ');
+
+					}
+				}
 			}
-			printf("\n");
 		}
 	}
+
 }
