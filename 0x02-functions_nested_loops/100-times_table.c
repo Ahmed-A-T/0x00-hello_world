c6ff591848816a944d9b11810bf0e11903b84dc6
Ahmed Abdelhafez <ahmedhafez.ta@gmail.com>
2024-07-24 01:08:26 +0300

    Debugging

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,6 +12,7 @@
 void print_times_table(int n)
 {
 	int i, j, result = 0;
+
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
@@ -22,35 +23,22 @@ void print_times_table(int n)
 				if (result < 10)
 				{
 					_putchar((result % 10) + '0');
-					if (j < n)
-					{
-						_putchar(',');
-						_putchar('	');
-
-					}
 				}
 				else if ((result > 9) && (result < 100))
 				{
 					_putchar((result / 10) + '0');
 					_putchar((result % 10) + '0');
-					if (j < n)
-					{
-						_putchar(',');
-						_putchar(' ');
-
-					}
 				}
-				else if (result > 99)
+				else
 				{
 					_putchar((result / 100) + '0');
 					_putchar((result / 10) % 10 + '0');
 					_putchar((result % 10) + '0');
-					if (j < n)
-					{
-						_putchar(',');
-						_putchar(' ');
-
-					}
+				}
+				if (j < n)
+				{
+					_putchar(',');
+					_putchar(' ');
 				}
 			}
 			_putchar('\n');
