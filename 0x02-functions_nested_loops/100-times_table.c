5b885c12977b737d9326ddebb6b37f449e83311a
Ahmed Abdelhafez <ahmedhafez.ta@gmail.com>
2024-07-25 10:39:36 +0300

    Debugging Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,43 +20,30 @@ void print_times_table(int n)
 			for (j = 0; j <= n; j++)
 			{
 				result = i * j;
-				if (result < 10)
+				if (result > 0 && result < 10)
 				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
 					_putchar((result % 10) + '0');
 				}
 				else if ((result > 9) && (result < 100))
 				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
 					_putchar((result / 10) + '0');
 					_putchar((result % 10) + '0');
 
 				}
 				else
 				{
+					_putchar(',');
+					_putchar(' ');
 					_putchar((result / 100) + '0');
 					_putchar((result / 10) % 10 + '0');
 					_putchar((result % 10) + '0');
-				}
-				if (j < n)
-				{
-					if (result >= 0 && result <= 9)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-					}
-					else if (result > 9 && result <= 99)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-
-					}
-					else
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
 				}
 			}
 			_putchar('\n');
