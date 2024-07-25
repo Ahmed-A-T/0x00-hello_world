cb83a195bc7630a3f2e8d7546eea9a0c43ad6d38
Ahmed Abdelhafez <ahmedhafez.ta@gmail.com>
2024-07-25 11:39:59 +0300

    Debugging Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,33 +20,39 @@ void print_times_table(int n)
 			for (j = 0; j <= n; j++)
 			{
 				result = i * j;
-			 	if (j != 0)
+			 	if (j == 0)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((result % 10) + '0');
-				}
-				else if (j == 0)
 					_putchar('0');
-				else if ((result > 9) && (result < 100))
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((result / 10) + '0');
-					_putchar((result % 10) + '0');
-
 				}
 				else
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((result / 100) + '0');
-					_putchar((result / 10) % 10 + '0');
-					_putchar((result % 10) + '0');
-				}
+					else if ((result >=0 && result < 10))
+					{
+
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar((result / 10) + '0');
+					}
+					else if ((result > 9) && (result < 100))
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar((result / 10) + '0');
+						_putchar((result % 10) + '0');
+
+					}
+					else
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar((result / 100) + '0');
+						_putchar((result / 10) % 10 + '0');
+						_putchar((result % 10) + '0');
+					}
+					}
 			}
 			_putchar('\n');
 		}
