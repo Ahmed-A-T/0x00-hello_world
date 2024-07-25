5966115ecbdb20ae9d4110db74cb74dffb511c7b
Ahmed Abdelhafez <ahmedhafez.ta@gmail.com>
2024-07-25 11:44:52 +0300

    Debugging Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -41,7 +41,6 @@ void print_times_table(int n)
 						_putchar(' ');
 						_putchar((result / 10) + '0');
 						_putchar((result % 10) + '0');
-
 					}
 					else
 					{
@@ -51,10 +50,9 @@ void print_times_table(int n)
 						_putchar((result / 10) % 10 + '0');
 						_putchar((result % 10) + '0');
 					}
-					}
+				}
 			}
 			_putchar('\n');
 		}
 	}
-
 }
