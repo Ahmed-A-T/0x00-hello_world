47eace071f4747f50c44a4a0e4d2220d8e04c3d8
Ahmed Abdelhafez <ahmedhafez.ta@gmail.com>
2024-07-30 00:34:10 +0300

    Comlete Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+/* 
+ * Filename: 6-print_line.c
+ * Author: Ahmed-A-T
+ */
+
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: the number of times the character _ should be printed
+ */
+void print_line(int n)
+{
+	int i;
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
