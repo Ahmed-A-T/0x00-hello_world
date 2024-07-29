3de07e2ac2a0205f5fe6d18747473f3dda1f9317
Ahmed Abdelhafez <ahmedhafez.ta@gmail.com>
2024-07-29 22:32:29 +0300

    Adding Function prototype for task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,11 +1,27 @@
+/*
+ * Filename: 5-more_numbers.c
+ * Author: Ahmed-A-T
+ */
+
 #include "main.h"
 
+/**
+ *  more_numbers - prints 10 times the numbers, from 0 to 14
+ */
 void more_numbers(void)
 {
-	int count;
+	int i, digit;
 
-	for (count = 0; count <= 19; count++)
+	for (i = 0; i < 10; i++)
 	{
-		_putchar
+		for (digit = 0; digit < 14; digit++)
+		{
+			if (digit > 9)
+				_putchar('1');
+			_putchar((digit % 10) + '0');
+		}
+		_putchar('0');
 	}
+
+
 }
