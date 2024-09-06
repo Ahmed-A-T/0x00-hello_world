14bd57f52167d7c4e09886e7c0b7aa8152c5eed3
Ahmed Abdelhafez <ahmedhafez.ta@gmail.com>
2024-09-06 19:17:56 +0300

    adding else if statement to print 0 if the number equal to 0

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -18,9 +18,11 @@ void print_number(int n)
 		_putchar('-');
 		n = -n;
 	}
+	else if (n == 0)
+		_putchar(n + '0');
 	while (n / divisor >= 10)
 		divisor *= 10;
-	while (n != 0)
+	while (n > 0)
 	{
 		digit = n / divisor;
 		_putchar(digit + '0');
